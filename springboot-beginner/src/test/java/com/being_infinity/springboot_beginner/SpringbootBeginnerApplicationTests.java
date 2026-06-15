e1ad7acb04f9b4d8e9ab95755f2593c9528b21ca
mrhacker-cpu <rockingsrujan1148@gmail.com>
2026-06-15 23:26:30 +0530

    project push

---
+++ springboot-beginner/src/test/java/com/being_infinity/springboot_beginner/SpringbootBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.being_infinity.springboot_beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
