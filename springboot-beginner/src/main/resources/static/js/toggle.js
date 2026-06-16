70694653b603c53c3f69a29608fe29238d292434
mrhacker-cpu <rockingsrujan1148@gmail.com>
2026-06-16 23:14:59 +0530

    new

---
+++ springboot-beginner/src/main/resources/static/js/toggle.js
@@ -0,0 +1,25 @@
+var users=[
+    {
+    "name":"John peter",
+    "gender":"male",
+    "image":"../../images/john.png"
+    },
+    {
+        "name":"jane",
+        "gender":"female",
+        "image":"../../images/jane.png"
+    }
+];
+
+var id=0;
+function toggle(){
+    id=(id+1)%2;
+    var user=users[id];
+
+    document.getElementById("user-name").innerHTML=user.name;
+    document.getElementById("user-gender").innerHTML=user.gender;
+    document.getElementById("user-image").src=user.image;
+
+    console.log(user);
+}
+
